57fcc5428406844c25b72fe40b7239f0596299de
hiyaera <mansoorinaaz2319@gmail.com>
2024-02-09 18:09:32 +0530

    login page testing

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function signup(){
+
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+localStorage.setItem("User-name",name)
+localStorage.setItem("User-email",email)
+localStorage.setItem("User-password",password)
+
+
+}
+
+
+// function login() {
+//     alert("want to login?");
+//   }
+
