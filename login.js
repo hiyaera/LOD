c4007271bab2a45ab1e770c20b0f8b9fce2a87d7
hiyaera <mansoorinaaz2319@gmail.com>
2024-02-12 17:37:14 +0530

    battery file added

--- login.js
+++ login.js
@@ -1,16 +1,18 @@
-function login(){
-
+function login()
+{
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
     var useremail = localStorage.getItem('useremail');
     var userpassword = localStorage.getItem('userpassword');
 
-    if( email==useremail && password==userpassword){
-        window.location.href="index.html";
-    } else{
+    if (email == useremail && password == userpassword)
+    {
+        window.location.href = "index.html";
+        alert("Redirecting to LockTheDeal")
+
+    } else
+    {
         alert("Email and Password doesn't match")
     }
-
-
 }
