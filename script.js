ce2dd4e68a8ae184226dfffb84983fdedf77d64c
hiyaera <mansoorinaaz2319@gmail.com>
2024-02-12 13:16:58 +0530

    responsive done

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
 
 
-
-// function signup(){
-
-// const name = document.getElementById('name').value;
-// const email = document.getElementById('email').value;
-// const password = document.getElementById('password').value;
-
-// localStorage.setItem("User-name",name)
-// localStorage.setItem("User-email",email)
-// localStorage.setItem("User-password",password)
-
-
-// }
-
-
-// function login() {
-//     alert("want to login?");
-//   }
-
