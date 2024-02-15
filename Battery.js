d99927cc31f1ddc0599c77a8d7b916560221bd14
hiyaera <mansoorinaaz2319@gmail.com>
2024-02-15 14:50:56 +0530

    checkbox updated

--- Battery.js
+++ Battery.js
@@ -1,11 +1,40 @@
 
-function checkbox() {
-    const checkboxes = document.querySelectorAll('input[name="category"]:checked');
-   
-    for(const data of checkboxes){
-        console.log(data.value);
-    }
-}
+// function checkbox(){
+//     const checkboxes = document.querySelectorAll('input[name="category"]:checked');
+//     const values = [];
+
+//     for (const data of checkboxes)  {
+//         values.push(data.value)
+//     }
+//     console.log(values);
+// }
+
+    // const checkboxes = document.querySelectorAll('input[name="category"]:checked');
+    // for(const data of checkboxes){
+    //     console.log(data.value);
+    // }
+
 
     
+    const values = [];
+    function checkbox(element) {
+
+        const elPos = values.indexOf(element.value);
+
+        if( elPos!= -1 && !element.checked){
+            values.splice(elPos,1)
+        }else{
+            values.push(element.value)
+        }
+        console.log(values);
+    }
+    
+
+
+
+
+   
+
+
+
 
